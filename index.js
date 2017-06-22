05191547235081a9564e8eae4ab1aede5c806ccd
filzilla <phillipdelia@Phillips-MacBook-Pro.local>
2017-06-22 18:57:25 -0400

    completed first page

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const mustacheExpress = require('mustache-express');
-const dataFile = require('/data.js')
+const bodyParser = require('body-parser');
+const dataFile = require('./data.js');
 
-
-const app = express();
-
+const application = express();
 
 
 
 // define model
-let robot = [
-    'id:' ,
-    'avatar:' ,
-    'name:' ,
-    'email:' ,
-    'university:',
-    'job:' ,
-    'address':[
-      'street_num:' ,
-      'street_name' ,
-      'city:'       ,
-      'state_or_province:' ,
-      'postal_code:'  ,
-      'country:'  ,
+// let robot = {};
 
-    ]
-];
+// robot.name = request.body.username;
+// robot.avatar = request.body.avatar;
+// robot.name = request.body.name;
+// robot.job = request.body.job;
+// robot.streetnum = request.body.address.street_num;
+// robot.steetname = request.body.address.street_name;
+// robot.city = request.body.address.city;
+// robot.state = request.body.address.state_or_province;
+// robot.postalcode = request.body.address.postal_code;
+// robot.country = request.body.address.country;
 
-let model = robot;
+// let model = robot;
 
-app.engine('mustache', mustacheExpress());
-app.set('views', './views')
-app.set('view engine', 'mustache')
+application.engine('mustache', mustacheExpress());
+application.set('view engine', 'mustache');
+application.set('views', './views');
+
+
+application.use(bodyParser.urlencoded());
+
+application.use(express.static('public'));
 
 //Listening on root
-app.get('/data.js', function (req, res) {
-  res.render('', model);
-})
+application.get('/', function (request, response) {
+  response.render('index', dataFile);
+});
 
-app.listen(3000, function () {
-  console.log('Successfully started express application!');
+application.get('/', function(request, response){
+  response.render('robot-card',dataFile);
 })
+//arrray protype filter
+
+application.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
